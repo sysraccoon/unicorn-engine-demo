719f8dd9967903acec8ea579b85a1e9d0d53352c
sysraccoon <sysraccoon@gmail.com>
2025-01-07 04:04:38 +0300

    use make to build quick-fib

--- quick-fib/quick-fib-package.nix
+++ quick-fib/quick-fib-package.nix
@@ -1,35 +1,12 @@
 {
   stdenv,
-  cmake,
 }:
 stdenv.mkDerivation {
   name = "quick-fib-demo";
   src = ./.;
 
-  nativeBuildInputs = [
-    cmake
+  installFlags = [
+    "DESTDIR=$(out)"
+    "PREFIX="
   ];
-
-  configurePhase = ''
-    mkdir -p build
-    cd build
-    cmake ../src
-  '';
-
-  buildPhase = ''
-    make
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp quick-fib-demo $out/bin
-
-    mkdir -p $out/lib
-    cp *.so $out/lib
-    # mkdir -p $out/lib
-    # cp libquick-fib.so $out/lib/libquick-fib.so
-    #
-    # mkdir -p $out/bin
-    # cp quick-fib-demo $out/bin/quick-fib
-  '';
 }
