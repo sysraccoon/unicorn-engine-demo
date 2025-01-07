22bc32944b8478b938057a3eb540cb0e7a7900ce
sysraccoon <sysraccoon@gmail.com>
2025-01-07 04:30:31 +0300

    synchronize doc_demo.rs output with unicorn_doc_demo.c

--- unicorn-rs-demo/src/doc_demo.rs
+++ unicorn-rs-demo/src/doc_demo.rs
@@ -5,8 +5,6 @@ const X86_CODE32: [u8; 2] = [0x41, 0x4a]; // INC ecx; DEC edx
 const ADDRESS: u64 = 0x1000000;
 
 pub fn main() {
-    println!("Emulate i386 code");
-
     let mut emu = Unicorn::new(Arch::X86, Mode::MODE_32)
         .expect("failed to initialize Unicorn instance");
 
@@ -19,6 +17,10 @@ pub fn main() {
     let r_ecx = 0x1234;
     let r_edx = 0x7890;
 
+    println!("original state:");
+    println!(">>> ECX = 0x{:02x}", r_ecx);
+    println!(">>> EDX = 0x{:02x}", r_edx);
+
     emu.reg_write(RegisterX86::ECX, r_ecx)
         .expect("failed to write to ECX register");
     emu.reg_write(RegisterX86::EDX, r_edx)
@@ -27,6 +29,7 @@ pub fn main() {
     emu.emu_start(ADDRESS, ADDRESS + (X86_CODE32.len() as u64), 0, 0)
         .expect("failed to emulate x86 code");
 
+    println!("emulation result:");
     println!(">>> ECX = 0x{:02x}", emu.reg_read(RegisterX86::ECX).unwrap());
     println!(">>> EDX = 0x{:02x}", emu.reg_read(RegisterX86::EDX).unwrap());
 }
